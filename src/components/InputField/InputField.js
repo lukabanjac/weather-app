f7655ac9ade97022d2855430fa4610df481f4961
Luka Banjac <banjacluka96@gmail.com>
2021-12-01 00:15:01 +0100

    added input field

--- src/components/InputField/InputField.js
+++ src/components/InputField/InputField.js
@@ -5,22 +5,24 @@ import SunAndCloudImage from "../../assets/sun_cloud.png";
 import ReactFlagsSelect from 'react-flags-select';
 
 const InputField = () => {
-  const [selected, setSelected] = useState('');
+  const [selected, setSelected] = useState('NL');
 
   useEffect(() => {
     console.log("Input Field loaded");
   }, [])
 
   return (
-    <div className="InputField">
-      <img className="SunAndCloud" src={SunAndCloudImage} alt="cloud and sun"></img>
+    <div className="inputField">
+      <img className="sunAndCloud" src={SunAndCloudImage} alt="Cloud And Sun"></img>
       <ReactFlagsSelect
         selected={selected}
         onSelect={code => setSelected(code)}
         customLabels={customLabels}
-        className
+        className="flagInput"
         searchable
       />
+
+      <input className="cityInput"></input>
     </div>
   )
 };
