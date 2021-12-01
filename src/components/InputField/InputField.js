b61939574b94ea5d27df274144be36d54ebde7a7
Luka Banjac <banjacluka96@gmail.com>
2021-12-02 00:23:48 +0100

    added API calls and parsed response data

--- src/components/InputField/InputField.js
+++ src/components/InputField/InputField.js
@@ -2,18 +2,32 @@ import React, { useState, useEffect } from 'react';
 import './InputField.scss';
 import customLabels from '../../assets/customLables';
 import SunAndCloudImage from "../../assets/sun_cloud.png";
+import SearchIcon from "../../assets/search.svg";
 import ReactFlagsSelect from 'react-flags-select';
+import Api from '../../helper/api';
+import Util from '../../helper/util';
 
 const InputField = () => {
+  const api = new Api();
+  const util = new Util();
   const [selected, setSelected] = useState('NL');
 
   useEffect(() => {
     console.log("Input Field loaded");
   }, [])
 
+  const getCityTemperature = () => {
+    api.getCityWeather("Novi Sad,RS").then(
+      res => {
+        //TODO: iteriraj kroz sve ovo i ispisi, ne zaboravi pokusati kao formu ovo napraviti i stilove za input i gradient. Enter na inputu
+        console.log(util.formatWeather(res.data.list))
+      }
+    ).catch(err => console.log("Error occoured while getteing weather: ", err));
+  }
+
   return (
     <div className="inputField">
-      <img className="sunAndCloud" src={SunAndCloudImage} alt="Cloud And Sun"></img>
+      <img className="sunAndCloud" src={SunAndCloudImage} alt="cloud and sun"></img>
       <ReactFlagsSelect
         selected={selected}
         onSelect={code => setSelected(code)}
@@ -22,7 +36,13 @@ const InputField = () => {
         searchable
       />
 
-      <input className="cityInput"></input>
+      <div className="cityInputWrapper">
+        <input className="cityInputField"></input>
+        <button className="citySearchButton" onClick={getCityTemperature}>
+          <img src={SearchIcon} alt="search"></img>
+        </button>  
+      </div>
+
     </div>
   )
 };
